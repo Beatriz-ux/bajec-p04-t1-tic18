b31d9ff5b9900bbd62ee9757bdfafc5f55f7bded
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-08-28 12:03:40 -0300

    att exercicio2

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <wchar.h>
 
 using namespace std;
 
 int main(void){
 
     char c = '0';
+    wchar_t wc;
+
     cout << "\tInt\tHex\tOct\n";
     cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
     cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
@@ -18,9 +21,8 @@ int main(void){
     cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
 
     cout << "Digite um char: ";
-    cin >> c;
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
-
+    wcin >> wc;
+    wcout << "'" << wc <<"' -\t" << (int)wc << '\t' << hex << (int)wc << '\t' << oct << (int)wc << '\t' << '\n';
 
     return 0;
 }
