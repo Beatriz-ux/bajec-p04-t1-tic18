bdffdffff5e3110b3b263ef4ef84af1caacc3a92
Joao Manoel <jmaoliveira.cic@uesc.br>
2023-08-28 09:36:43 -0300

    add arquivos das atvds

---
+++ src/exercicio2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+
+    char c = '0';
+    cout << "\tInt\tHex\tOct\n";
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
+
+    cout << "Digite um char: ";
+    cin >> c;
+    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
+
+
+    return 0;
+}
