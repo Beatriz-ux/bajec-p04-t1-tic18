d5fd59cdf263eecae3e7f09c2fa94e3da18ecd9a
Everaldina G. Barbosa <everaldinag@gmail.com>
2023-08-29 19:31:18 -0300

    feat item b

--- src/exercicio3.cpp
+++ src/exercicio3.cpp
@@ -4,7 +4,12 @@
 using namespace std;
 
 int main(){
+    unsigned long int uli = numeric_limits<unsigned long>::max();
+
     cout << "Limite superior do tipo >int< : " << numeric_limits<int>::max() << endl;
     cout << "Limite inferior do tipo >int< : " << numeric_limits<int>::min() << endl;
+    cout << "Limite superior do tipo >usigned long int< : " << uli << endl;
+    cout << "Limite inferior do tipo >usigned long int< : " << numeric_limits<unsigned long>::min() << endl;
+
     return 0;
 }
