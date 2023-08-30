a7dbbf58f361a6c07649383b2c1855e281a2ca81
João Manoel <jmaoliveira.cic@uesc.br>
2023-08-29 22:27:21 -0300

    att exercicio 2

--- src/exercicio2.cpp
+++ src/exercicio2.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 #include <wchar.h>
+#include <locale.h>
 
 using namespace std;
 
 int main(void){
+    setlocale(LC_ALL,"");
 
     char c = '0';
     wchar_t wc;
 
-    cout << "\tInt\tHex\tOct\n";
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
-    cout << "'" << c <<"' -\t" << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
+    cout << "\tDec\tHex\tOct\n";
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c++ << '\t' << '\n';
+    cout << "'" << c <<"' -\t" << dec << (int)c << '\t' << hex << (int)c << '\t' << oct << (int)c << '\t' << '\n';
 
-    cout << "Digite um char: ";
+
+    cout << "Digite um caractere char: ";
     wcin >> wc;
-    wcout << "'" << wc <<"' -\t" << (int)wc << '\t' << hex << (int)wc << '\t' << oct << (int)wc << '\t' << '\n';
+
+    wcout << "'" << wc <<"' -\t" << dec << (int)wc << '\t' << hex << (int)wc << '\t' << oct << (int)wc << '\t' << '\n';
 
     return 0;
 }
